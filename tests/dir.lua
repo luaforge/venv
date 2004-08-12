f87d15a85413907c7fb89a799eeb0774fde53f2d
cvs2svn <admin@example.com>
2004-08-12 04:13:20 +0000

    This commit was manufactured by cvs2svn to create tag 'v1_0a'.

--- tests/dir.lua
+++
@@ -1,8 +0,0 @@
--- $Id: dir.lua,v 1.1 2004-05-31 14:23:59 ana Exp $
-if not dir and loadlib then
-	local libname = "libdir.1.0a.so"
-	local libopen = "luaopen_dir"
-	local init, err1, err2 = loadlib (libname, libopen)
-	assert (init, (err1 or '')..(err2 or ''))
-	init ()
-end
