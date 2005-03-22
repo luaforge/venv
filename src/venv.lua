521b4a8b4bef166088c1ebd972de24cbfaff370f
Tomas Guisasola <tomas@luaforge.net>
2005-03-22 10:08:12 +0000

    Cleaning unused functions.

--- src/venv.lua
+++ src/venv.lua
@@ -1,5 +1,5 @@
 ----------------------------------------------------------------------------
--- $Id: venv.lua,v 1.7 2005-03-22 10:04:16 tomas Exp $
+-- $Id: venv.lua,v 1.8 2005-03-22 10:08:12 tomas Exp $
 ----------------------------------------------------------------------------
 
 local ipairs, pairs = ipairs, pairs
@@ -23,30 +23,6 @@ local function getcontrol(t, key)
 end
 
 local function pack (...) return arg end
-
---
--- auxiliar function to read "nested globals"
---
-local function getfield (t, f)
-  for w in string.gfind(f, "[%w_]+") do
-    if not t then return nil end
-    t = t[w]
-  end
-  return t
-end
-
-
---
--- auxiliar function to write "nested globals"
---
-local function setfield (t, f, v)
-  for w in string.gfind(f, "([%w_]+)%.") do
-    t[w] = t[w] or {} -- create table if absent
-    t = t[w]            -- get the table
-  end
-  local w = string.gsub(f, "[%w_]+%.", "")   -- get last field name
-  t[w] = v            -- do the assignment
-end
 
 
 ----------------------------------------------------------------------------
@@ -87,119 +63,6 @@ newcontrolledtable = function (inherit)
                       __newindex = newIndex})
   createcontrol(newt)
   return newt
-end
-
-----------------------------------------------------------------------------
--- Redefinition of 'require', allowing libraries to be stored in the
--- global environment but executed within a virtual environment
-----------------------------------------------------------------------------
-
-local function getpath(ng)
-  local path = ng._PATH
-  if type(path) ~= "string" then
-    path = os.getenv("LUA_PATH")
-    if path == nil then
-      path = "?;?.lua"
-    end
-  end
-  return path
-end
-
-local function search (path, name)
-  for c in string.gfind(path, "[^;]+") do
-    c = string.gsub(c, "%?", name)
-    local f = io.open(c)
-    if f then   -- file exist?
-      f:close()
-      return c
-    end
-  end
-  return nil
-end
-
-local function new_require_and_module(ng, currg)
-  local _p = currg.package
-  local loaded = newcontrolledtable(_p.loaded)
-  -- build a copy of `package' table
-  local package = newcontrolledtable(_p)
-  package.path = _p.path
-  package.cpath = _p.cpath
-  package.loaded = loaded
-  package.loaders = _p.loaders
-  ng.package = package
-  local module = _G.module
-  local getfield, setfield = getfield, setfield
-
-  -- redefine `require'
-  local new_req = function(name)
-    assert(type(name) == "string", string.format(
-      "bad argument #1 to `require' (string expected, got %s)", type(name)))
-    local p = getfield(loaded, name)
-    if p then return p end
-    -- first mark it as loaded
-    setfield(loaded, name, true)
-    --local f = load(name)
-    --setfield(preload, name, f)
-    local actual_arg = arg
-    arg = { name }
-    loaded[name] = load(name)(name) or true
-    --loaded[name] = f(name) or true
-    arg = actual_arg
-    return getfield(loaded, name)
-  end
---[[
-    -- test if module was loaded in this environment
-    if not loaded[name] then
-      loaded[name] = true
-      -- test if the module is stored in the global environment
-      local f = preload[name]
-      if not f then
-        local filename = string.gsub(name, "%.", "/")
-        local fullname = search(package.cpath, filename)
-        if fullname then
-          local openfunc = "luaopen_"..string.gsub(name, "%.", "")
-          f = assert(loadlib(fullname, openfunc))
-        else
-          fullname = search(package.path, filename)
-          if not fullname then
-            error("cannot find "..name.." in path "..package.path.." nor in "..package.cpath, 2)
-          end
-          f = assert(loadfile(fullname))
-        end
-        -- store module in the global environment
-        preload[name] = f
-      end
-      -- run module inside virtual environment if not loaded yet
-      pcall (setfenv, f, ng)
-      local old_arg = ng.arg
-      ng.arg = { name }
-      local res = f(name)
-      ng.arg = old_arg
-      if res then loaded[name] = res end
-    end
-    return loaded[name]
-  end
---]]
-  -- redefine `module'
-  local new_mod = function(name)
-    local _G = ng
-    local ns = getfield(_G, name)
-    if not ns then
-      ns = {}
-      setfield(_G, name, ns)
-    elseif type(ns) ~= "table" then
-      error("name conflict for module `"..name.."'")
-    end
-    if not ns._NAME then
-      ns._NAME = name
-      ns._PACKAGE = string.gsub(name, "[^.]*$", "")
-    end
-    setmetatable(ns, {__index = _G})
-    loaded[name] = ns
-    setfenv(2, ns)
-    return ns
-  end
-  return new_req, new_mod
 end
 
 local function clonetable (t, n)
